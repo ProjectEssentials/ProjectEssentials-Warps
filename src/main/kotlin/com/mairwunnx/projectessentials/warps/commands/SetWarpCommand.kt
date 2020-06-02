03c7a1abf776b880a22061a1b4923e65be6d9625
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-02 23:16:31 +0300

    Set-warp command re-written.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

--- src/main/kotlin/com/mairwunnx/projectessentials/warps/commands/SetWarpCommand.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/warps/commands/SetWarpCommand.kt
@@ -1,84 +1,44 @@
 package com.mairwunnx.projectessentials.warps.commands
 
-import com.mairwunnx.projectessentials.cooldown.essentials.CommandsAliases
-import com.mairwunnx.projectessentials.core.extensions.isPlayerSender
-import com.mairwunnx.projectessentials.core.helpers.throwOnlyPlayerCan
-import com.mairwunnx.projectessentials.core.helpers.throwPermissionLevel
-import com.mairwunnx.projectessentials.warps.EntryPoint
-import com.mairwunnx.projectessentials.warps.EntryPoint.Companion.hasPermission
-import com.mairwunnx.projectessentials.warps.models.WarpModel
-import com.mairwunnx.projectessentials.warps.models.WarpModelUtils
-import com.mairwunnx.projectessentials.warps.sendMessage
-import com.mojang.brigadier.CommandDispatcher
-import com.mojang.brigadier.arguments.StringArgumentType
-import com.mojang.brigadier.builder.LiteralArgumentBuilder.literal
+import com.mairwunnx.projectessentials.core.api.v1.MESSAGE_MODULE_PREFIX
+import com.mairwunnx.projectessentials.core.api.v1.commands.CommandAPI
+import com.mairwunnx.projectessentials.core.api.v1.commands.CommandBase
+import com.mairwunnx.projectessentials.core.api.v1.extensions.currentDimensionId
+import com.mairwunnx.projectessentials.core.api.v1.extensions.getPlayer
+import com.mairwunnx.projectessentials.core.api.v1.messaging.MessagingAPI
+import com.mairwunnx.projectessentials.core.api.v1.messaging.ServerMessagingAPI
+import com.mairwunnx.projectessentials.warps.configurations.WarpsConfigurationModel
+import com.mairwunnx.projectessentials.warps.helpers.validateAndExecute
+import com.mairwunnx.projectessentials.warps.warpsConfiguration
 import com.mojang.brigadier.context.CommandContext
 import net.minecraft.command.CommandSource
-import net.minecraft.command.Commands
-import org.apache.logging.log4j.LogManager
 
-object SetWarpCommand {
-    private val aliases = listOf(
-        "setwarp", "esetwarp", "createwarp", "ecreatewarp"
-    )
-    private val logger = LogManager.getLogger()
+object SetWarpCommand : CommandBase(setWarpLiteral, false) {
+    override val name = "set-warp"
+    override fun process(context: CommandContext<CommandSource>) = 0.also {
+        fun out(status: String, vararg args: String) = MessagingAPI.sendMessage(
+            context.getPlayer()!!, "${MESSAGE_MODULE_PREFIX}warps.setwarp.$status", args = *args
+        )
 
-    fun register(dispatcher: CommandDispatcher<CommandSource>) {
-        logger.info("Register \"/setwarp\" command")
-        applyCommandAliases()
-
-        aliases.forEach { command ->
-            dispatcher.register(
-                literal<CommandSource>(command).then(
-                    Commands.argument(
-                        "warp name", StringArgumentType.string()
-                    ).executes {
-                        return@executes execute(it)
-                    }
-                )
-            )
-        }
-    }
-
-    private fun applyCommandAliases() {
-        if (EntryPoint.cooldownsInstalled) {
-            CommandsAliases.aliases["setwarp"] = aliases.toMutableList()
-        }
-    }
-
-    private fun execute(c: CommandContext<CommandSource>): Int {
-        if (c.isPlayerSender()) {
-            val player = c.source.asPlayer()
-            if (hasPermission(player, "ess.warp.set")) {
-                val warpName = StringArgumentType.getString(c, "warp name")
-                val clientWorld = c.source.world.worldInfo.worldName
-                val worldId = c.source.world.worldType.id
-                val xPos = player.posX.toInt()
-                val yPos = player.posY.toInt()
-                val zPos = player.posZ.toInt()
-                val yaw = player.rotationYaw
-                val pitch = player.rotationPitch
-                WarpModelUtils.warpModel.warps.forEach {
-                    if (it.name == warpName) {
-                        sendMessage(c.source, "exist", warpName)
-                        return 0
+        validateAndExecute(context, "ess.warp.set", 0) { isServer ->
+            if (isServer) {
+                ServerMessagingAPI.throwOnlyPlayerCan()
+            } else {
+                val name = CommandAPI.getString(context, "warp")
+                warpsConfiguration.warps.asSequence().find {
+                    it.name == name
+                }?.let { out("exist", name) } ?: run {
+                    with(context.getPlayer()!!) {
+                        warpsConfiguration.warps.add(
+                            WarpsConfigurationModel.Warp(
+                                name, this.name.string,
+                                currentDimensionId, position.x, position.y, position.z,
+                                rotationYaw, rotationPitch
+                            )
+                        ).also { out("success").also { super.process(context) } }
                     }
                 }
-                WarpModelUtils.warpModel.warps.add(
-                    WarpModel.Warp(
-                        warpName, clientWorld, player.name.string, worldId,
-                        xPos, yPos, zPos, yaw, pitch
-                    )
-                )
-                sendMessage(c.source, "set.success", warpName)
-                logger.info("Executed command \"/setwarp\" from ${player.name.string}")
-            } else {
-                sendMessage(c.source, "set.restricted")
-                throwPermissionLevel(player.name.string, "setwarp")
             }
-        } else {
-            throwOnlyPlayerCan("setwarp")
         }
-        return 0
     }
 }

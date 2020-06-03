207a01310ad838832df42db4bed05c6114f0a21b
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-03 08:40:00 +0300

    Del-warp command suggestion improved.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

--- src/main/kotlin/com/mairwunnx/projectessentials/warps/commands/CommandLiterals.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/warps/commands/CommandLiterals.kt
@@ -1,5 +1,9 @@
 package com.mairwunnx.projectessentials.warps.commands
 
+import com.mairwunnx.projectessentials.core.api.v1.extensions.getPlayer
+import com.mairwunnx.projectessentials.core.api.v1.extensions.isPlayerSender
+import com.mairwunnx.projectessentials.core.api.v1.extensions.playerName
+import com.mairwunnx.projectessentials.core.api.v1.permissions.hasPermission
 import com.mairwunnx.projectessentials.warps.warpsConfiguration
 import com.mojang.brigadier.arguments.StringArgumentType
 import com.mojang.brigadier.builder.LiteralArgumentBuilder
@@ -29,9 +33,19 @@ inline val delWarpLiteral: LiteralArgumentBuilder<CommandSource>
     get() = LiteralArgumentBuilder.literal<CommandSource>("del-warp").then(
         Commands.argument(
             "warp", StringArgumentType.string()
-        ).suggests { _, builder ->
+        ).suggests { ctx, builder ->
             ISuggestionProvider.suggest(
-                warpsConfiguration.warps.asSequence().map { it.name }.toList(), builder
+                if (ctx.isPlayerSender()) {
+                    if (hasPermission(ctx.getPlayer()!!, "ess.warp.remove.other", 4)) {
+                        warpsConfiguration.warps.asSequence().map { it.name }.toList()
+                    } else {
+                        warpsConfiguration.warps.asSequence().filter {
+                            it.owner == ctx.playerName()
+                        }.map { it.name }.toList()
+                    }
+                } else {
+                    warpsConfiguration.warps.asSequence().map { it.name }.toList()
+                }, builder
             )
         }.executes { DelWarpCommand.process(it) }
     )

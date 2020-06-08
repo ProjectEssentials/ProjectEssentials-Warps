b23ff996f46afc62e31a8722e34ab114d5ed7cf1
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-08 18:11:50 +0300

    configure-warps command literal added.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

--- src/main/kotlin/com/mairwunnx/projectessentials/warps/commands/CommandLiterals.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/warps/commands/CommandLiterals.kt
@@ -5,14 +5,17 @@ import com.mairwunnx.projectessentials.core.api.v1.extensions.isPlayerSender
 import com.mairwunnx.projectessentials.core.api.v1.extensions.playerName
 import com.mairwunnx.projectessentials.core.api.v1.permissions.hasPermission
 import com.mairwunnx.projectessentials.warps.warpsConfiguration
+import com.mojang.brigadier.arguments.BoolArgumentType
+import com.mojang.brigadier.arguments.IntegerArgumentType
 import com.mojang.brigadier.arguments.StringArgumentType
 import com.mojang.brigadier.builder.LiteralArgumentBuilder
+import com.mojang.brigadier.builder.LiteralArgumentBuilder.literal
 import net.minecraft.command.CommandSource
 import net.minecraft.command.Commands
 import net.minecraft.command.ISuggestionProvider
 
 inline val warpLiteral: LiteralArgumentBuilder<CommandSource>
-    get() = LiteralArgumentBuilder.literal<CommandSource>("warp").then(
+    get() = literal<CommandSource>("warp").then(
         Commands.argument(
             "warp", StringArgumentType.string()
         ).suggests { _, builder ->
@@ -23,14 +26,14 @@ inline val warpLiteral: LiteralArgumentBuilder<CommandSource>
     )
 
 inline val setWarpLiteral: LiteralArgumentBuilder<CommandSource>
-    get() = LiteralArgumentBuilder.literal<CommandSource>("set-warp").then(
+    get() = literal<CommandSource>("set-warp").then(
         Commands.argument(
             "warp", StringArgumentType.string()
         ).executes { SetWarpCommand.process(it) }
     )
 
 inline val delWarpLiteral: LiteralArgumentBuilder<CommandSource>
-    get() = LiteralArgumentBuilder.literal<CommandSource>("del-warp").then(
+    get() = literal<CommandSource>("del-warp").then(
         Commands.argument(
             "warp", StringArgumentType.string()
         ).suggests { ctx, builder ->
@@ -49,3 +52,40 @@ inline val delWarpLiteral: LiteralArgumentBuilder<CommandSource>
             )
         }.executes { DelWarpCommand.process(it) }
     )
+
+inline val configureWarpsLiteral: LiteralArgumentBuilder<CommandSource>
+    get() = literal<CommandSource>("configure-warps")
+        .then(
+            Commands.literal("play-sound-on-teleport").then(
+                Commands.literal("set").then(
+                    Commands.argument("value", BoolArgumentType.bool()).executes {
+                        ConfigureWarpsCommand.playSoundOnTeleport(it)
+                    }
+                )
+            )
+        ).then(
+            Commands.literal("show-effect-on-teleport").then(
+                Commands.literal("set").then(
+                    Commands.argument("value", BoolArgumentType.bool()).executes {
+                        ConfigureWarpsCommand.showEffectOnTeleport(it)
+                    }
+                )
+            )
+        ).then(
+            Commands.literal("add-resistance-effect").then(
+                Commands.literal("set").then(
+                    Commands.argument("value", BoolArgumentType.bool()).executes {
+                        ConfigureWarpsCommand.addResistanceEffect(it)
+                    }
+                )
+            )
+        ).then(
+            Commands.literal("resistance-effect-duration").then(
+                Commands.literal("set").then(
+                    Commands.argument("value", IntegerArgumentType.integer(1)).executes {
+                        ConfigureWarpsCommand.resistanceEffectDuration(it)
+                    }
+                )
+            )
+        )
+
